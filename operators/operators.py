e33912950f673744bef7384b21c5fbbae25e817f
Syed Talal Jilani <talaljilani79@gmail.com>
2023-09-11 18:15:30 +0500

    all operator is added

--- operators/operators.py
+++ operators/operators.py
@@ -1,3 +1,40 @@
 # arithematic (+,/,*,-,%)
 a = 10 + 20
 print(a)#30
+b = 10-30
+print(b)# -20
+c = 10/2
+print(c)#5.0 fraction
+d = 10%2
+print(d)#reminder is zero
+e= 10*20
+print(e)#200
+# logical (and,or,not)
+first=10
+second=-20
+third = 200
+#and
+if first == 10 and second==-20:
+    print('True')
+# or
+if(first==200 or third==200):
+    print('2True')
+# not
+if not first==100:
+    print('3True')
+
+# relational (>,<,==,!=,>=,<=,)
+age = int(input('Enter Your Age'))
+if age >= 18:
+    print('You are Eligible');
+elif age< 18:
+    print('You are Not Eligible');
+
+number = 0;
+# uninary operator is not available 
+# increment 
+number+=1 # number=number+1
+print(number)# 1 increment 0+1 = 1
+# decrement
+number-=1
+print(number)# 1-1 = 0 
